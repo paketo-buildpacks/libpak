8b0d1e0667e54bce3e00482e52b012b6bae5abf0
Ben Hale <bhale@pivotal.io>
2020-02-28 16:51:20 -0800

    File Listing
    This change creates a Sherpa function for creating a file listing for a given
    directory.  This listing contains an entry for each file and directory, and
    each of those entries contains the path, permissions, modified time, and
    SHA256 hash of the original file.
    
    Signed-off-by: Ben Hale <bhale@pivotal.io>

---
+++ sherpa/file_listing.go
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2018-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sherpa
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
+	"sync"
+	"time"
+)
+
+// FileEntry is metadata about a file.
+type FileEntry struct {
+
+	// Path is the path of a file.
+	Path string `toml:"path"`
+
+	// Mode is the mode of the source file.
+	Mode string `toml:"mode"`
+
+	// ModificationTime is the modification time of the file.
+	ModificationTime time.Time `toml:"modification-time"`
+
+	// SHA256 is the SHA256 has of the source file.
+	SHA256 string `toml:"sha256,omitempty"`
+}
+
+type result struct {
+	err   error
+	value FileEntry
+}
+
+// NewFileListing generates a listing of all entries under root.
+func NewFileListing(root string) ([]FileEntry, error) {
+	ch := make(chan result)
+	var wg sync.WaitGroup
+
+	if err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if path == root {
+			return nil
+		}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			e := FileEntry{
+				Path:             path,
+				Mode:             info.Mode().String(),
+				ModificationTime: info.ModTime(),
+			}
+
+			if info.IsDir() {
+				ch <- result{value: e}
+				return
+			}
+
+			s := sha256.New()
+
+			in, err := os.Open(path)
+			if err != nil {
+				ch <- result{err: fmt.Errorf("unable to open file %s: %w", path, err)}
+				return
+			}
+			defer in.Close()
+
+			if _, err := io.Copy(s, in); err != nil {
+				ch <- result{err: fmt.Errorf("unable to hash file %s: %w", path, err)}
+				return
+			}
+
+			e.SHA256 = hex.EncodeToString(s.Sum(nil))
+			ch <- result{value: e}
+		}()
+
+		return nil
+	}); err != nil {
+		return nil, fmt.Errorf("error walking path %s: %w", root, err)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	var e []FileEntry
+	for r := range ch {
+		if r.err != nil {
+			return nil, fmt.Errorf("unable to create file listing: %s", r.err)
+		}
+
+		e = append(e, r.value)
+	}
+	sort.Slice(e, func(i, j int) bool {
+		return e[i].Path < e[j].Path
+	})
+
+	return e, nil
+}
---
+++ sherpa/file_listing_test.go
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sherpa_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	"github.com/paketoio/libpak/sherpa"
+	"github.com/sclevine/spec"
+)
+
+func testFileListing(t *testing.T, context spec.G, it spec.S) {
+	var (
+		Expect = NewWithT(t).Expect
+
+		path string
+	)
+
+	it.Before(func() {
+		var err error
+
+		path, err = ioutil.TempDir("", "file-listing")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	it.After(func() {
+		Expect(os.RemoveAll(path)).To(Succeed())
+	})
+
+	it("create listing", func() {
+		Expect(ioutil.WriteFile(filepath.Join(path, "alpha.txt"), []byte{}, 0644)).To(Succeed())
+		Expect(os.MkdirAll(filepath.Join(path, "test-directory"), 0755)).To(Succeed())
+		Expect(ioutil.WriteFile(filepath.Join(path, "test-directory", "bravo.txt"), []byte{}, 0644)).To(Succeed())
+
+		e, err := sherpa.NewFileListing(path)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(e).To(HaveLen(3))
+	})
+}
