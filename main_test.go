78a9475e0271cbc65f5c5decc48954699dcd7b9b
Ben Hale <bhale@vmware.com>
2020-07-02 11:45:56 -0700

    Dependency Updates
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- main_test.go
+++ main_test.go
@@ -118,6 +118,8 @@ test-key = "test-value"
 
 		exitHandler = &mocks.ExitHandler{}
 		exitHandler.On("Error", mock.Anything)
+		exitHandler.On("Pass", mock.Anything)
+		exitHandler.On("Fail", mock.Anything)
 
 		layersPath, err = ioutil.TempDir("", "main-layers-path")
 		Expect(err).NotTo(HaveOccurred())
@@ -201,9 +203,30 @@ test-key = "test-value"
 			libcnb.WithArguments([]string{commandPath, platformPath, buildPlanPath}),
 			libcnb.WithExitHandler(exitHandler),
 		)
+	})
 
-		Expect(exitHandler.Calls).To(BeEmpty())
+	it("calls exitHandler.Pass() on detection pass", func() {
+		detector.On("Detect", mock.Anything).Return(libcnb.DetectResult{Pass: true}, nil)
+		commandPath := filepath.Join("bin", "detect")
 
+		libpak.Main(detector, builder,
+			libcnb.WithArguments([]string{commandPath, platformPath, buildPlanPath}),
+			libcnb.WithExitHandler(exitHandler),
+		)
+
+		Expect(exitHandler.Calls[0].Method).To(BeIdenticalTo("Pass"))
+	})
+
+	it("calls exitHandler.Fail() on detection fail", func() {
+		detector.On("Detect", mock.Anything).Return(libcnb.DetectResult{Pass: false}, nil)
+		commandPath := filepath.Join("bin", "detect")
+
+		libpak.Main(detector, builder,
+			libcnb.WithArguments([]string{commandPath, platformPath, buildPlanPath}),
+			libcnb.WithExitHandler(exitHandler),
+		)
+
+		Expect(exitHandler.Calls[0].Method).To(BeIdenticalTo("Fail"))
 	})
 
 	it("encounters an unknown command", func() {
